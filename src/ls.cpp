b0ee28b9461382bba99ba9e2ba95f2c7cf9f723f
Hannes <aphil013@ucr.edu>
2015-05-01 15:18:05 -0700

    Added plenty of error checking. Early part of opening and reading the directory seems to work as well

--- src/ls.cpp
+++ src/ls.cpp
@@ -33,6 +33,68 @@ bool compare(const string& s1, const string& s2)	// Comparison for sort function
 vector<string> listDir(int flag, string init)
 {
 	vector<string> dir;
+	vector<string> files;
+	vector<struct stat> info;
+
+	struct stat statbuf;
+	struct stat lstatbuf;
+	struct pswd *pwd;
+	struct group *grp;
+
+	string dir_path(init);
+	dir_path.append("/"); // Format for directory output
+	string copy(dir_path);
+
+	const char* directory_name = init.c_str();
+	DIR *dir_ptr = opendir(directory_name);
+	if(dir_ptr == NULL)
+	{
+		perror("opendir"); // Error checking
+		return dir;
+	}
+	
+	dirent *dirent_ptr;
+	while(dirent_ptr = readdir(dir_ptr))
+		files.push_back(dirent_ptr->d_name);
+	
+	if(errno != 0)
+	{
+		perror("readdir"); // Error while reading directory
+		exit(1); // Exit after error
+	}
+
+	sort(files.begin(), files.end(), compare);
+
+	for(unsigned int i = 0; i < files.size(); ++i)
+	{
+		dir_path.append(files.at(i));
+		if((lstat(dir_path.c_str(), &lstatbuf) == -1))
+		{
+			perror("lstat");
+			exit(1);
+		}
+		
+		if(S_ISLNK(lstatbuf.st_mode))
+		{
+			info.push_back(lstatbuf);
+			dir_path = copy;
+			continue;
+		}
+
+		if((stat(dir_path.c_str(), &statbuf)) == -1)
+		{
+			perror("stat");
+			exit(1);
+		}
+		
+		info.push_back(statbuf);
+
+		if((files.at(i) != ".") && (files.at(i) != "..") && (S_ISDIR(statbuf.st_mode)))
+		{
+			dir.push_back(dir_path);
+		}
+		dir_path = copy;
+	}
 
 	return dir;
 }
