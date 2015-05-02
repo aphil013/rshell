15a6054710b10ecfc72fdbbc08c3487d61765b65
Hannes <aphil013@ucr.edu>
2015-05-01 17:16:50 -0700

    Finalized other flags and no flags.Program seems to be complete. Testing soon

--- src/ls.cpp
+++ src/ls.cpp
@@ -38,7 +38,7 @@ vector<string> listDir(int flag, string init)
 
 	struct stat statbuf;
 	struct stat lstatbuf;
-	struct pswd *pwd;
+	struct passwd *pwd;
 	struct group *grp;
 
 	string dir_path(init);
@@ -155,6 +155,168 @@ vector<string> listDir(int flag, string init)
 
 	}
 
+	else if(flag & FLAG_l)
+	{
+		int block = 0;
+		for(unsigned int i = 0; i < files.size(); ++i)
+		{
+			if(!(flag & FLAG_a) && files.at(i)[0] == '.')
+				continue;
+			else
+				block += info.at(i).st_blocks;
+		}
+		cout << "total: " << block/2 << endl;
+
+		for(unsigned int i = 0; i < files.size(); ++i)
+		{
+			if(files.at(i)[0] == '.' && !(flag && FLAG_a))
+				continue;
+			else
+			{
+				if(S_ISDIR(info.at(i).st_mode))
+					cout << 'd';
+				else if(S_ISLNK(info.at(i).st_mode))
+					cout << 'l';
+				else
+					cout << '-';
+
+				//Permissions
+				
+				//user
+				if(info.at(i).st_mode & S_IRUSR)
+					cout << 'r';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IWUSR)
+					cout << 'w';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IXUSR)
+					cout << 'x';
+				else cout << '-';
+
+				//Group
+				if(info.at(i).st_mode & S_IRGRP)
+					cout << 'r';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IWGRP)
+					cout << 'w';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IXGRP)
+					cout << 'x';
+				else cout << '-';
+
+				//Other
+				if(info.at(i).st_mode & S_IROTH)
+					cout << 'r';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IWOTH)
+					cout << 'w';
+				else cout << '-';
+				if(info.at(i).st_mode & S_IXOTH)
+					cout << 'x';
+				else cout << '-';
+
+				cout << " " << info.at(i).st_nlink << " ";
+
+				pwd = getpwuid(info.at(i).st_uid);
+				cout << pwd->pw_name << " ";
+
+				grp = getgrgid(info.at(i).st_gid);
+				cout << grp->gr_name << " ";
+
+				cout << info.at(i).st_size << " ";
+
+				time_t time = info.at(i).st_mtime;
+				string time_s = ctime(&time);
+				cout << time_s.substr(4, 12) << " ";
+
+				if(files.at(i)[0] == '.')
+					cout << H_COLOR;
+				if(S_ISDIR(info.at(i).st_mode))
+				{
+					cout << D_COLOR;
+					cout << files.at(i);
+					cout << '/';
+				}
+				else
+				{
+					if(S_ISLNK(info.at(i).st_mode));
+					else if(info.at(i).st_mode & S_IEXEC)
+						cout << E_COLOR;
+
+					cout << files.at(i);
+				}
+
+				cout << RESET_C;
+
+				if(!(i + 1 == files.size()))
+					cout << endl;
+			}
+		}
+		cout << endl;
+	}
+
+	else
+	{
+		unsigned width = 80;
+		unsigned longest = 0;
+
+		for(unsigned int i = 0; i < files.size(); ++i)
+		{
+			if(files.at(i).size() > longest)
+				longest = files.at(i).size();
+		}
+
+		longest += 2;
+		unsigned columns = width/longest;
+		unsigned curr = 0;
+
+		for(unsigned int i = 0; i < files.size(); ++i)
+		{
+			if(files.at(i)[0] == '.')
+				continue;
+			else
+			{
+				dir_path.append(files.at(i));
+				if((stat(dir_path.c_str(), &statbuf)) == -1)
+				{
+					perror("stat");
+					exit(1);
+				}
+
+				if(S_ISDIR(statbuf.st_mode))
+				{
+					cout << D_COLOR;
+					cout << files.at(i) << "/";
+					curr = files.at(i).size() + 1;
+					cout << RESET_C;
+				}
+
+				else
+				{
+					if(statbuf.st_mode & S_IEXEC)
+						cout << E_COLOR;
+
+					cout << files.at(i);
+					curr = files.at(i).size();
+					cout << RESET_C;
+				}
+
+				cout << string(longest - curr, ' ');
+				columns--;
+				if(columns == 0)
+				{
+					columns = width/longest;
+				}
+				
+				dir_path = copy;
+			}
+
+			if((columns == width/longest && columns != 0) || (i+1) == files.size())
+				cout << endl;
+		}
+	}
+
+				
 	return dir;
 }
 
