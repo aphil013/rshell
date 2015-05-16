764a3a7ed9a3fc587aebddf19e33575b7c0cc236
Hannes <aphil013@ucr.edu>
2015-05-15 17:14:06 -0700

    Edited format of original rshell. Overhauled structure Implemented functions for execution and tokenizing
    Function that splits vector containing commands into two seperated
    by the piping character, |
    
    Normal bash commands still work. A change has caused connectors to
    fail. Identifying is next priority.
    
    Remaining function where redirection actually takes place upcoming

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -26,42 +26,91 @@ void user_prompt(string& user)				// Gets user info for command prompt
 	user = name + host;
 }
 
-bool execute(char** commands)					// Executes and returns success value 
+vector<string> tok(char x[], string key)		// Redone nicer tokens
 {
-	if(*commands == NULL)
+	const char* str = key.c_str(); // Error without const
+	char* cmd = strtok(x, str);
+	vector<string> z;
+	while(cmd != '\0')
 	{
-		return false;
+		z.push_back(cmd);
+		cmd = strtok('\0', str);
 	}
-	
-	int status = 0;
-	int pid = fork();
-	if(pid == -1)
+	return z;
+}
+
+void execute(vector<string> commands)
+{
+	unsigned int size = commands.size();
+	char** arg = new char*[size+1];
+	arg[size] = '\0';
+	for(unsigned int i = 0; i < size; ++i)
 	{
-		perror("fork");
+		arg[i] = new char[commands.at(i).size()];
+		strcpy(arg[i], commands.at(i).c_str());
+	}
+
+	if(execvp(arg[0], arg) == -1)
+	{
+		delete[] arg;
+		perror("execvp");
 		exit(1);
 	}
-	else if(pid == 0)
-	{
-		status = execvp(commands[0], commands);
-		if(status == -1)
-		{
-			perror("execvp");
-			exit(1);
-		}
-	}	
+	return;
+}
+
+void execution(vector<string> commands)
+{
+	vector<string> sec1;	// Sections of connected commands
+	vector<string> sec2;
+
+	unsigned int i;
+	for(i = 0; i < commands.size() && commands.at(i) != "|"; ++i)
+		sec1.push_back(commands.at(i));
+	for(i += 1; i < commands.size(); ++i)
+		sec2.push_back(commands.at(i));
+	
+	if(sec2.size() == 0)	// No pipe present
+		execute(commands);
 	else
 	{
-		int wait = waitpid(pid, &status, 0);
-		if(wait == -1)
-			perror("wait");
-		string cond = commands[0];
-		if(cond == "true") return true;
-		if(cond == "false") return false;
-		
-			
+		int ps[2];
+		if(pipe(ps) == -1)		// Pipe sys call
+			perror("pipe");  
+		int pid = fork();
+		if(pid == -1)
+		{
+			perror("fork");
+			exit(1);
+		}
+		else if(pid == 0)
+		{
+			if(dup2(ps[1], 1) == -1)
+				perror("dup2");
+			if(close(ps[0]) == -1)
+				perror("close");
+			execute(sec1);
+		}
+		else					// Tons of error checking in parent
+		{
+			int stat = 0;
+			if(waitpid(-1, &stat, 0) == -1)
+				perror("wait");
+			int rest = dup(0);
+			if(rest == -1)
+				perror("dup");
+			if(dup2(ps[0], 0) == -1)
+				perror("dup2");
+			if(close(ps[1]) == -1)
+				perror("close");
+			execution(sec2);	// Recursive call on second section
+			if(dup2(rest, 0) == -1)
+				perror("dup2");
+		}
 	}
-	return true;
+	return;
 }
+
 void replace(string &str, const string& curr, const string& hip)
 {
 	size_t pos = 0;
@@ -72,7 +121,7 @@ void replace(string &str, const string& curr, const string& hip)
 	}
 	return;
 }
-void redirect_spacer(string &str)
+void redirect_spacer(string &str)			// Calls replace to make parsing redirection simpler
 {
 	replace(str, "<", " < ");
 	replace(str, ">", " > ");
@@ -88,15 +137,12 @@ int main()
 	user_prompt(user);		// Gets login/host info
 
 	string cmd_line;
-	int array_sz = 0x8000;
 
 	while(cin.good())		// Continuous loop simulating terminal
 	{
-		bool prev = true;
+		begin:								// Label to jump to
+		//bool prev = true;
 
-		bool re_in = false;
-		bool re_out = false;
-		bool re_pipe = false;
 
 		cout << user << "$ ";				// Prompt and input 
 		getline(cin, cmd_line);
@@ -121,128 +167,112 @@ int main()
 				x = semi + 2;
 			}
 		}
-	/*	
+		
 		char*  cmd_str = new char [cmd_line.size()+1];				// Create array of c-strings
 		strcpy(cmd_str, cmd_line.c_str());
-		char** commands = new char*[array_sz];
-
-		
-
-		string key; // Connector storage
-		string logic; // Command storage
+		vector<string> commands;
+		string key = ""; // Connector storage
+	
 		string io;
-		unsigned int i = 0;
-		char* cmd = strtok(cmd_str, " \t");			// Begin parsing
 
-		while(cmd != NULL)						// First command apart from connectors
+		if(cmd_line.find(";") != string::npos)
 		{
-			logic = cmd;
-			if(logic == "||" || logic == "&&" || logic == ";")
-				break;	
-			else{		
-				commands[i] = cmd;
-				++i;
-				cmd = strtok(NULL, " \t");
-			}
+			commands = tok(cmd_str, ";");
+			key = ";";
 		}
-		key = logic;
-		if(commands[0] != NULL)						// Executes first command
+		else if(cmd_line.find("&&") != string::npos)
 		{
+			commands = tok(cmd_str, "&&");
+			key = "&&";
+		}
+		else if(cmd_line.find("||") != string::npos)
+		{
+			commands = tok(cmd_str, "||");
+			key = "||";
+		}
+		
+		if(key == "")
+		{
+			int stat = 0;
+			commands = tok(cmd_str, " ");
 
-			for(unsigned int k = 0; k < i; ++k)
+			if(commands.size() == 1 && commands.at(0) == "exit")
 			{
-				io = commands[k];
-				if(io == "<") re_in = true;
-				else if(io == ">") re_out = true;
-				else if(io == "|") re_pipe = true;
-			}
-			logic = commands[0];
-			if(logic == "exit")
+				delete[] cmd_str;
 				return 0;
-			if(logic == "")
-				prev = false;
+			}
+			int pid = fork();
+			if(pid == -1)
+			{
+				perror("fork");
+				exit(1);
+			}
+			else if(pid == 0) // Child process
+				execution(commands);
 			else
-				if(!re_in && !re_out && !re_pipe)
-					prev = execute(commands);
+			{
+				if(waitpid(-1, &stat, 0) == -1)
+				{
+					perror("waitpid");
+					exit(1);
+				}
 				else
 				{
-					if(re_in && !re_out && !re_pipe)
-					{
+					delete[] cmd_str;
+					goto begin;
+				}
+			}
+		}
 
+		vector<vector<string> > super;	// Vector to be populated for cmds
+		for(unsigned int i = 0; i < commands.size(); ++i)
+		{
+			char* alpha = new char[commands.at(i).size()+1];
+			strcpy(alpha, commands.at(i).c_str());
+			super.push_back(tok(alpha, " "));
+		}
+		for(unsigned int i = 0; i < super.size(); ++i)
+		{
+			int stat = 0;
+			if(super.at(i).size() == 1 && super.at(i).at(0) == "exit")
+			{
+				delete[] cmd_str;
+				return 0;
+			}
+			int pid = fork();
+			if(pid == -1)
+			{
+				perror("fork");
+				exit(1);
+			}
+			else if(pid == 0)
+				execution(commands);
+			else
+			{
+				if(waitpid(-1, &stat, 0) == -1)
+				{
+					perror("waitpid");
+					exit(1);
+				}
+				else
+				{
+					if(key == "")
+						break;
+					else if(key == ";");
+					else if(key == "||")
+					{
+						if(stat <= 0)
+							break;
+					}
+					else if(key == "&&")
+					{
+						if(stat > 0)
+							break;
 					}
 				}
-					
-		}
-		for(unsigned int j = 0; j < sizeof(commands); ++j)		// For loops to "erase" executed command
-		{
-			commands[j] = '\0';
-		}
-
-		cmd = strtok(NULL, " \t");
-		i = 0;		
-		while(cmd != NULL)						// Everything else w/ connector calculation
-		{
-			logic = cmd;
-			if(logic == "exit")						// Exit at anytime
-				return 0;
-
-			if(logic == "") prev = false;
-			else if(logic != "||" && logic != "&&" && logic != ";")		// Continue parsing if no connectors reached
-			{
-				commands[i] = cmd;
-				++i;
 			}
-			else if(logic == "||")						// Connectors determine each execution path
-			{
-				key = logic;
-				logic = commands[0];
-				if(logic == "exit")
-					return 0;
-				if(!prev) prev = execute(commands);
-				else prev = 0;
-				for(unsigned int j = 0; j < sizeof(commands); ++j)
-				{
-					commands[j] = '\0';
-				}
-				i = 0;
-			}
-			else if(logic == "&&")
-			{
-				key = logic;
-				logic = commands[0];
-				if(logic == "exit")
-					return 0;
-				if(prev) prev = execute(commands);
-				for(unsigned int j = 0; j < sizeof(commands); ++j)
-				{
-					commands[j] = '\0';
-				}
-				i = 0;
-			}
-			else if(logic == ";")
-			{
-				prev = true; 
-					
-				for(unsigned int j = 0; j < sizeof(commands); ++j)
-				{
-					commands[j] = '\0';
-				}
-				i = 0;
-			}
-			cmd = strtok(NULL, " \t");
-	
 		}
-		if(i != 0)									// Execute last command, if any
-		{
-			if(key == "||")
-				if(!prev) execute(commands);
-			if(key == "&&")
-				if(prev) execute(commands);
-			if(key == ";") execute(commands);
-		}
-			
-	}*/
-}
-	return 0;
+		delete[] cmd_str;
+	}
 
 }
